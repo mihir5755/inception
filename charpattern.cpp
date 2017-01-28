50328282a1d5e537a99304ac4262d76b38b9c7a0
mihir5755 <mihirp5755@gmail.com>
2017-01-28 10:06:55 +0530

    Add files via upload

---
+++ charpattern.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,z=1; 
+	cout<<"Enter rows: ";
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=2*(n-i);j++)
+		cout<<" ";
+	for(int k=1;k<=2*i-1;k++,z++)
+	cout<<(char)(z+64)<<" ";
+	cout<<"\n";
+	}
+}
+
